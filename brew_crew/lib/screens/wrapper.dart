d623bf457baa84f03705df5923a4a8859dfc1b95
Jackson Geer <jacksongeer@gmail.com>
2023-11-02 19:13:21 -0400

    added basic app structure

---
+++ brew_crew/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return either Home or Authenticate widget
+    return Home();
+  }
+}
